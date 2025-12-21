ca54b0f4591ac34b8bd5fac3388a0f0174f07a97
Astro251 <www.astrogamer@gmail.com>
2025-12-21 20:58:35 +0530

    Update moviebox.js

--- moviebox.js
+++ moviebox.js
@@ -1,2 +1,8 @@
-const video = document.querySelector('video');
-video.muted = false;
+document.body.addEventListener('click', () => {
+    const video = document.querySelector('video');
+    if (video) {
+        if (video.muted) {
+            video.muted = false;
+        }
+    }
+},
