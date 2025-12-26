a380fe3f3df5470f3bade13ff1cbc7a81b8eaf8a
Astro251 <www.astrogamer@gmail.com>
2025-12-26 15:54:04 +0530

    Update moviebox.js

--- moviebox.js
+++ moviebox.js
@@ -1,14 +1,1 @@
-document.body.addEventListener('click', () => {
-    const video = document.querySelector('video');
-    if (video) {
-        video.muted = false;
-        
-        if (video.requestFullscreen) {
-            video.requestFullscreen();
-        } else if (video.webkitRequestFullscreen) { /* Safari */
-            video.webkitRequestFullscreen();
-        } else if (video.msRequestFullscreen) { /* IE11 */
-            video.msRequestFullscreen();
-        }
-    }
-},);
+
